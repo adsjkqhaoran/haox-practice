254290d3e57968f042d2cab94b0d0c91358bebeb
adsjkqhaoran <adsjkqhaoran@qq.com>
2017-07-10 11:21:26 +0800

    add message

--- algorithm/sort/mergesort.js
+++ algorithm/sort/mergesort.js
@@ -4,8 +4,8 @@ var mergeSortRec = function (array) {
         return array;
     }
     var mid = Math.floor(length / 2),
-        left = array.slice(0, mid),
-        right = array.slice(mid, length);
+        left = array.slice(0, mid),//slice 是截取[需要注意的是slice,两个参数分别是开始,结束!!!但是不包含结束 很坑]  splice 为删除并返回 
+        right = array.slice(mid +1 , length - 1);
     return merge(mergeSortRec(left), mergeSortRec(right));
 }
 var merge = function (left, right) {
