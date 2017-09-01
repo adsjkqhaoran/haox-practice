9f4a70c60be24791c59c0bcf7bed365907e73543
haox <adsjkqhaoran>
2017-09-01 17:26:59 +0800

    add my mind

--- algorithm/sort/mergesort.js
+++ algorithm/sort/mergesort.js
@@ -4,7 +4,7 @@ var mergeSortRec = function (array) {
         return array;
     }
     var mid = Math.floor(length / 2),
-        left = array.slice(0, mid),//slice 是截取[需要注意的是slice,两个参数分别是开始,结束!!!但是不包含结束 很坑]  splice 为删除并返回 
+        left = array.slice(0, mid),//slice 是截取[需要注意的是slice,两个参数分别是开始,个数  splice 为删除并返回
         right = array.slice(mid +1 , length - 1);
     return merge(mergeSortRec(left), mergeSortRec(right));
 }
@@ -35,4 +35,7 @@ console.log(mergeSortRec([8, 5, 7, 6, 9, 3, 4, 2, 1]));
 //8    5    67   9    3    4    2     1
 //58   679  34   12
 //56789  1234
-//123456789  
+//123456789
+
+//这个和冒泡法比较 该方法存在最理想情况 而冒泡则不存在
+//假设4个数进行排序 冒泡法铁定要比较3+2+1=6次 而组合归并排序最快是4次 最慢是5次
